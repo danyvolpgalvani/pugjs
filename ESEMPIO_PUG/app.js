fb7fda4a3e2df5bedcd6a1be7d324c7f69d52c02
Daniele Volpato <volpato.daniele@iisgalvanimi.edu.it>
2019-11-18 10:01:08 +0000

    02.1.2

--- ESEMPIO_PUG/app.js
+++ ESEMPIO_PUG/app.js
@@ -1,16 +1,17 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const people = require('./people.json'); //Copia il file people.json dentro la variabile people
+const app = express();
 
-app.set('view engine', 'pug');   //Dico a express di usare pug come motore di template
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
 
-app.use(express.static(__dirname + '/public')); // Dico ad express dove recuperare i file statici
-
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.render('index', {
-   title: 'Homepage',
-   content : 'Questa pagina parla del mondo e di tanto altro'
- });
+    title: 'Homepage',
+    people: people.profiles //Passa il vettore profiles alla pagina index.pug
+  });
 });
+
 app.listen(3000, function () {
  console.log('Example app listening on port 3000!');
 });
