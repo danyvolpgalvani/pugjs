88664e03019db22fc74314a22f4a2261522bb4ee
Daniele Volpato <volpato.daniele@iisgalvanimi.edu.it>
2019-11-18 09:49:44 +0000

    02.1.1

--- ESEMPIO_PUG/app.js
+++ ESEMPIO_PUG/app.js
@@ -6,8 +6,10 @@ app.set('view engine', 'pug');   //Dico a express di usare pug come motore di te
 app.use(express.static(__dirname + '/public')); // Dico ad express dove recuperare i file statici
 
 app.get('/', function (req, res) {
- //res.send('Ciao Mondo');
- res.render('index');  //Dico a express di processare e inviare la pagina index.pug
+  res.render('index', {
+   title: 'Homepage',
+   content : 'Questa pagina parla del mondo e di tanto altro'
+ });
 });
 app.listen(3000, function () {
  console.log('Example app listening on port 3000!');
